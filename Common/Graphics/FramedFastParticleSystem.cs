211d8d7e3b17d745c727e5fa8823ce0087619bfc
Ouarissem <ouarissemchenna79@gmail.com>
2025-10-25 11:13:58 -0700

    Add files via upload

---
+++ Common/Graphics/FramedFastParticleSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Luminance.Core.Graphics;
+using Microsoft.Xna.Framework;
+using NoxusBoss.Core.Graphics.FastParticleSystems;
+using Terraria.ModLoader;
+using MatrixSIMD = System.Numerics.Matrix4x4;
+using Vector2SIMD = System.Numerics.Vector2;
+
+namespace IdolOfMadderCrimson.Common.Graphics;
+
+[Autoload(Side = ModSide.Client)]
+public class FramedFastParticleSystem(int totalFrames, int maxParticles, Action renderPreparations, FastParticleSystem.ParticleUpdateAction? extraUpdates = null) :
+    FastParticleSystem(maxParticles, renderPreparations, extraUpdates)
+{
+    /// <summary>
+    ///     The amount of frames this particle has.
+    /// </summary>
+    public readonly int TotalFrames = totalFrames;
+
+    protected override void PopulateVertexBufferIndex(VertexPosition2DColorTexture[] vertices, int particleIndex)
+    {
+        ref FastParticle particle = ref particles[particleIndex];
+
+        float fadeIn = LumUtils.InverseLerp(0f, 36f, particle.Time);
+        Color color = (particle.Active ? particle.Color : Color.Transparent) * fadeIn;
+        Vector2SIMD center = Unsafe.As<Vector2, Vector2SIMD>(ref particle.Position);
+        Vector2SIMD size = Unsafe.As<Vector2, Vector2SIMD>(ref particle.Size) * fadeIn;
+
+        float rotationX = Math.Clamp(particle.Rotation * 1.1f, -0.3f, 0.3f);
+        float rotationY = Math.Clamp(particle.Rotation * 0.5f, -0.3f, 0.3f);
+        MatrixSIMD particleRotationMatrix = MatrixSIMD.CreateRotationX(rotationX) *
+                                            MatrixSIMD.CreateRotationY(rotationY) *
+                                            MatrixSIMD.CreateRotationZ(particle.Rotation);
+
+        Vector2SIMD topLeftPosition = center + Vector2SIMD.Transform(topLeftOffset * size, particleRotationMatrix);
+        Vector2SIMD topRightPosition = center + Vector2SIMD.Transform(topRightOffset * size, particleRotationMatrix);
+        Vector2SIMD bottomLeftPosition = center + Vector2SIMD.Transform(bottomLeftOffset * size, particleRotationMatrix);
+        Vector2SIMD bottomRightPosition = center + Vector2SIMD.Transform(bottomRightOffset * size, particleRotationMatrix);
+
+        int frameY = particleIndex % TotalFrames;
+        float topY = frameY / (float)TotalFrames;
+        float bottomY = (frameY + 1f) / TotalFrames;
+
+        int vertexIndex = particleIndex * 4;
+        vertices[vertexIndex] = new VertexPosition2DColorTexture(Unsafe.As<Vector2SIMD, Vector2>(ref topLeftPosition), color, Vector2.UnitY * topY, frameY);
+        vertices[vertexIndex + 1] = new VertexPosition2DColorTexture(Unsafe.As<Vector2SIMD, Vector2>(ref topRightPosition), color, new Vector2(1f, topY), frameY);
+        vertices[vertexIndex + 2] = new VertexPosition2DColorTexture(Unsafe.As<Vector2SIMD, Vector2>(ref bottomRightPosition), color, new Vector2(1f, bottomY), frameY);
+        vertices[vertexIndex + 3] = new VertexPosition2DColorTexture(Unsafe.As<Vector2SIMD, Vector2>(ref bottomLeftPosition), color, Vector2.UnitY * bottomY, frameY);
+    }
+}
---
+++ Common/Graphics/ParticleEngine.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Renderers;
+using Terraria.ModLoader;
+
+namespace IdolOfMadderCrimson.Common.Graphics;
+
+public class ParticleEngine : ILoadable
+{
+    /// <summary>
+    ///     Renders over dusts.
+    /// </summary>
+    public static readonly ParticleRenderer Particles = new ParticleRenderer();
+
+    /// <summary>
+    ///     Clears all particle contents.
+    /// </summary>
+    public static void Clear() => Particles.Clear();
+
+    public void Load(Mod mod)
+    {
+        On_Main.UpdateParticleSystems += UpdateParticles;
+        On_Main.DrawDust += DrawParticles;
+    }
+
+    private void UpdateParticles(On_Main.orig_UpdateParticleSystems orig, Main self)
+    {
+        orig(self);
+        Particles.Update();
+    }
+
+    private void DrawParticles(On_Main.orig_DrawDust orig, Main self)
+    {
+        orig(self);
+
+        Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+        Particles.Settings.AnchorPosition = -Main.screenPosition;
+        Particles.Draw(Main.spriteBatch);
+        Main.spriteBatch.End();
+    }
+
+    public void Unload() { }
+}
